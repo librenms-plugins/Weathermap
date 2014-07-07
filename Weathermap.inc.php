494d7166842504fb3f905cc6e7d4b0b3fc072e70
librenms-plugins <neil.lathwood@gmail.com>
2014-07-07 16:09:41 +0100

    Updated page for correct link

--- Weathermap.inc.php
+++ Weathermap.inc.php
@@ -1,5 +1,5 @@
 <?php
 
-echo ('Please go to the <a href="plugins/".get_class()."editor.php">editor</a> to manage your Weathermaps');
+echo ('Please go to the <a href="plugins/Weathermap/editor.php">editor</a> to manage your Weathermaps');
 
 ?>
