e55f8ec80c9ed6d86851be0e4395373ab726afaa
librenms-plugins <neil.lathwood@gmail.com>
2014-07-12 00:45:15 +0100

    Escaped $ in message

--- Weathermap.inc.php
+++ Weathermap.inc.php
@@ -1,6 +1,6 @@
 <?php
 
-print_message("Please ensure that you enable the editor for use, to do this please edit editor.php and set $ENABLED=true. Using Weathermap as a LibreNMS plugin means editor.php is actually using LibreNMS to authenticate anyway so unlike Weathermap on it's own, you don't actually need to disabled the editor when it's not in use.");
+print_message("Please ensure that you enable the editor for use, to do this please edit editor.php and set \$ENABLED=true. Using Weathermap as a LibreNMS plugin means editor.php is actually using LibreNMS to authenticate anyway so unlike Weathermap on it's own, you don't actually need to disabled the editor when it's not in use.");
 echo ('Please go to the <a href="plugins/Weathermap/editor.php">editor</a> to manage your Weathermaps');
 
 ?>
