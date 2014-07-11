f67e119214c7659039136238e0fe234ec067ed0c
librenms-plugins <neil.lathwood@gmail.com>
2014-07-12 00:54:21 +0100

    Added check for writeable config directory

--- Weathermap.inc.php
+++ Weathermap.inc.php
@@ -1,5 +1,9 @@
 <?php
 
+if(! is_writable('configs'))
+{
+  print_error("The map config directory is not writable by the web server user. You will not be able to edit any files until this is corrected.");
+}
 print_message("Please ensure that you enable the editor for use, to do this please edit editor.php and set \$ENABLED=true. Using Weathermap as a LibreNMS plugin means editor.php is actually using LibreNMS to authenticate anyway so unlike Weathermap on it's own, you don't actually need to disabled the editor when it's not in use.");
 echo ('Please go to the <a href="plugins/Weathermap/editor.php">editor</a> to manage your Weathermaps');
 
