5c27c43983d9b6ce5278a87e52d9b726ba5d1949
librenms-plugins <neil.lathwood@gmail.com>
2014-07-12 17:51:43 +0100

    Updated included php file to include README.md

--- Weathermap.inc.php
+++ Weathermap.inc.php
@@ -4,14 +4,14 @@ if(! is_writable('plugins/Weathermap/configs'))
 {
   print_error("The map config directory is not writable by the web server user. You will not be able to edit any files until this is corrected.");
 }
-print_message("Please ensure that you enable the editor for use, to do this please edit editor.php and set \$ENABLED=true. Using Weathermap as a LibreNMS plugin means editor.php is actually using LibreNMS to authenticate anyway so unlike Weathermap on it's own, you don't actually need to disabled the editor when it's not in use.");
-echo ('Please go to the <a href="plugins/Weathermap/editor.php">editor</a> to manage your Weathermaps');
+echo ('Click <a href="plugins/Weathermap/editor.php">here to access the editor</a> where you can create and manage maps. <h3>Please ensure you follow the installation instructions below.</h3>');
+$readme = @file_get_contents('README.md');
 echo('<div class="panel panel-default">
   <div class="panel-heading">
     <h3 class="panel-title">Installation instructions</h3>
   </div>
   <div class="panel-body">
-    <iframe src="plugins/Weathermap/INSTALL.md"></iframe>
+    '.$readme.'
   </div>
 </div>');
 
