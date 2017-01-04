bfd7941cf2e52dd054628c8448c393cc6fdf649d
kse <sebastian.koehlmeier@kyberna.com>
2017-01-04 10:17:02 +0100

    set path to non docker version

--- config.php
+++ config.php
@@ -1,6 +1,7 @@
 <?php
 
-//$librenms_base = realpath(__DIR__ . '/../../..');
-$librenms_base = '/opt/librenms/';
+$librenms_base = realpath(__DIR__ . '/../../..');
+//this is for the docker version
+//$librenms_base = '/opt/librenms/';
 $mapdir='configs';
 $librenms_url = '/';
