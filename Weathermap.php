01b92d37a000089fec087aed5593febcdc63ef8b
PipoCanaja <pipocanaja@gmail.com>
2018-07-11 16:09:18 +0200

    Conditionnal display of submenu if maps are defined

--- Weathermap.php
+++ Weathermap.php
@@ -5,18 +5,31 @@ include_once 'lib/editor.inc.php';
 class Weathermap {
 
   public static function menu() {
+        //Include config
+        include_once 'config.inc.php';
 
-include_once 'config.inc.php';
+	//Parse config files
+        $files = list_weathermaps($mapdir);
 
-	echo('<li class="dropdown-submenu"><a href="plugin/p='.get_class().'">'.get_class().'</a>');
-	$files = list_weathermaps($mapdir);
-	echo (' <ul class="dropdown-menu scrollable-menu">');
+	//Create submenu 
+	$submenu = ' <ul class="dropdown-menu scrollable-menu">';
+	$count = 0;
 	foreach ($files as $file=>$data) {
             $nicefile = htmlspecialchars($file);
-            echo '<li><a href="/plugins/'.get_class().'/'.htmlspecialchars($data['page']).'"><i class="fa fa-map fa-fw fa-lg" aria-hidden="true"></i> '.htmlspecialchars($data['title']).'</a></li>';
+            $submenu .= '   <li><a href="/plugins/'.get_class().'/'.htmlspecialchars($data['page']).'"><i class="fa fa-map fa-fw fa-lg" aria-hidden="true"></i> '.htmlspecialchars($data['title']).'</a></li>';
+            $count ++;
         }
-	echo (' </ul>');
-   	echo ('</li>');
+	$submenu .= ' </ul>';
+
+        //Display it if not empty
+        if ($count > 0) {
+            echo('<li class="dropdown-submenu"><a href="plugin/p='.get_class().'">'.get_class().'</a>');
+            echo $submenu;
+            echo ('</li>');
+        } else {
+            //Create menu without submenu
+            echo('<li><a href="plugin/p='.get_class().'">'.get_class().'</a></li>');
+        }
   }
 }
 ?>
