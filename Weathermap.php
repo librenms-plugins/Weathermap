a97e2743d6f46a200ce2ac1c5ff5553a35b4ef1b
PipoCanaja <pipocanaja@gmail.com>
2018-07-17 18:23:14 +0200

    A few more consequences of the Namespace

--- Weathermap.php
+++ Weathermap.php
@@ -16,21 +16,29 @@ class Weathermap {
 	//Create submenu 
 	$submenu = ' <ul class="dropdown-menu scrollable-menu">';
 	$count = 0;
+
+        //getShortName - might make sense to include this ShortName code into Plugin class somewhere ?
+	if ($pos = strrpos(get_class(), '\\')) { 
+            $short= substr(get_class(), $pos + 1);
+        } else { 
+            $short = $pos;
+        };
+
 	foreach ($files as $file=>$data) {
             $nicefile = htmlspecialchars($file);
-            $submenu .= '   <li><a href="/plugins/'.get_class().'/'.htmlspecialchars($data['page']).'"><i class="fa fa-map fa-fw fa-lg" aria-hidden="true"></i> '.htmlspecialchars($data['title']).'</a></li>';
+            $submenu .= '   <li><a href="/plugins/'.$short.'/'.htmlspecialchars($data['page']).'"><i class="fa fa-map fa-fw fa-lg" aria-hidden="true"></i> '.htmlspecialchars($data['title']).'</a></li>';
             $count ++;
         }
 	$submenu .= ' </ul>';
 
         //Display it if not empty
         if ($count > 0) {
-            echo('<li class="dropdown-submenu"><a href="plugin/p='.get_class().'">Weathermap</a>');
+            echo('<li class="dropdown-submenu"><a href="plugin/p='.$short.'">'.$short'.</a>');
             echo $submenu;
             echo ('</li>');
         } else {
             //Create menu without submenu
-            echo('<li><a href="plugin/p='.get_class().'">Weathermap</a></li>');
+            echo('<li><a href="plugin/p='.$short.'">'.$short.'</a></li>');
         }
   }
 }
